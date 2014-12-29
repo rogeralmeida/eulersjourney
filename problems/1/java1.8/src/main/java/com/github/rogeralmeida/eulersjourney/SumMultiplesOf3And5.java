18dd2c3cee288b74b932fda42b1398af4850f48b
Roger Almeida <roger.eduardo@gmail.com>
2014-12-28 23:09:07 -0200

    JAva and Python versions of problem 1

--- problems/1/java1.8/src/main/java/com/github/rogeralmeida/eulersjourney/SumMultiplesOf3And5.java
+++ problems/1/java1.8/src/main/java/com/github/rogeralmeida/eulersjourney/SumMultiplesOf3And5.java
@@ -4,7 +4,14 @@ package com.github.rogeralmeida.eulersjourney;
  * Created by ralmeid on 12/25/14.
  */
 public class SumMultiplesOf3And5 {
-    public Integer sum(int i , int i1) {
-        return 3;
+
+    public static void main(String... args){
+        Integer sum = 0;
+        for(int number=1; number < 1000; number++){
+            if (number % 3 == 0 || number % 5 == 0){
+                sum += number;
+            }
+        }
+        System.out.printf("The sum of multiples of 3 and 5 below 1000 is %d", sum);
     }
 }
---
+++ problems/1/python2/multiples.py
@@ -0,0 +1,2 @@
+multiples = sum([number for number in range(1, 1000) if (number % 3 == 0) or (number % 5 == 0)])
+print "The sum of multiples of 3 and 5 bellow 1000 is {}".format(multiples)
