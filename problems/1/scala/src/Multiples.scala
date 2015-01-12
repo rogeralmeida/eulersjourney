5476702b0e7cdeea6826441ada39ad6155317808
Roger Almeida <roger.eduardo@gmail.com>
2015-01-12 20:57:19 -0200

    Refactoring Scala solution for problem 1

--- problems/1/scala/src/Multiples.scala
+++ problems/1/scala/src/Multiples.scala
@@ -1,11 +1,8 @@
 object Multiples {
   def main (args: Array[String]) {
-    var number = 0;
-    var sum = 0;
-    for(number <- 1 to 999 if number % 3 == 0 || number % 5 == 0){
-      sum += number;
-    }
-    printf("The sum of multiples of 3 and 5 bellow 1000 is %d", sum);
+    val total = (1 to 999).toList.filter(number => number % 3 == 0 || number % 5 == 0).
+      reduce[Int]((accumulator, number) => accumulator+number)
+    printf("The sum of multiples of 3 and 5 bellow 1000 is %d", total);
   }
 
 }
