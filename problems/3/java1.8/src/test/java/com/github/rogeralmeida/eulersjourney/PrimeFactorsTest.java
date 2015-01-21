12395dd297260935f3b40364ff27d07253f692f8
Roger Almeida <roger.eduardo@gmail.com>
2015-01-21 20:36:27 -0200

    Added 315 and 360 scenarios

--- problems/3/java1.8/src/test/java/com/github/rogeralmeida/eulersjourney/PrimeFactorsTest.java
+++ problems/3/java1.8/src/test/java/com/github/rogeralmeida/eulersjourney/PrimeFactorsTest.java
@@ -2,6 +2,8 @@ package com.github.rogeralmeida.eulersjourney;
 
 import java.util.Arrays;
 
+import org.junit.Test;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -10,5 +12,15 @@ public class PrimeFactorsTest {
     @org.junit.Test
     public void calculate_shouldCalculate12() {
         assertThat(PrimeFactors.calculate(12), equalTo(Arrays.asList(2, 2, 3)));
+    }
+
+    @Test
+    public void calculate_shouldCalculate315() {
+        assertThat(PrimeFactors.calculate(315), equalTo(Arrays.asList(3, 3, 5, 7)));
+    }
+
+    @Test
+    public void calculate_shouldCalculate360() {
+        assertThat(PrimeFactors.calculate(360), equalTo(Arrays.asList(2, 2, 2, 3, 3, 5)));
     }
 }
